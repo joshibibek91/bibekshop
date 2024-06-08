ad3ec8fdc51006dd23a784fe84ee8e297a1ddb36
Bibek <bibek.joshi91@gmail.com>
2024-06-08 22:29:01 +0545

    sales

--- app/routes/app.bestseller.jsx
+++ app/routes/app.bestseller.jsx
@@ -7,60 +7,27 @@ import {
   Page,
   Text,
   BlockStack,
+  Grid,
+  LegacyCard,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 
 export default function BestSellerPage() {
   return (
-    <Page>
-      <TitleBar title="Best Seller" />
-      <Layout>
-        <Layout.Section>
-          <Card>
-            <BlockStack gap="300">
-              <Text as="p" variant="bodyMd">
-                The app template comes with an additional page which
-                demonstrates how to create multiple pages within app navigation
-                using{" "}
-                <Link
-                  url="https://shopify.dev/docs/apps/tools/app-bridge"
-                  target="_blank"
-                  removeUnderline
-                >
-                  App Bridge
-                </Link>
-                .
-              </Text>
-              <Text as="p" variant="bodyMd">
-                To create your own page and have it show up in the app
-                navigation, add a page inside <Code>app/routes</Code>, and a
-                link to it in the <Code>&lt;NavMenu&gt;</Code> component found
-                in <Code>app/routes/app.jsx</Code>.
-              </Text>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section variant="oneThird">
-          <Card>
-            <BlockStack gap="200">
-              <Text as="h2" variant="headingMd">
-                Resources
-              </Text>
-              <List>
-                <List.Item>
-                  <Link
-                    url="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-                    target="_blank"
-                    removeUnderline
-                  >
-                    App nav best practices
-                  </Link>
-                </List.Item>
-              </List>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-      </Layout>
+    <Page fullWidth>
+      <TitleBar title="Best Sales" />
+      <Grid>
+        <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+          <LegacyCard title=" Best Sales Products" sectioned>
+            <p>View a summary of your online store’s sales.</p>
+          </LegacyCard>
+        </Grid.Cell>
+        {/* <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+          <LegacyCard title="Orders" sectioned>
+            <p>View a summary of your online store’s orders.</p>
+          </LegacyCard>
+        </Grid.Cell> */}
+      </Grid>
     </Page>
   );
 }
